3b276f2d82a24e386f6cbdf9ec9686ad2749a8c5
sartaj <ssaarrttaajj111@gmail.com>
2026-01-26 23:17:56 +0530

    added cname to be copied

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -2,5 +2,7 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy("CNAME");
+
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };
