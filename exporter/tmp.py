fb7e74254549a4b77575d4034a38c2e72bdc5be1
sartaj <mail@sartaj.in>
2026-08-03 13:52:20 +0530

    some chsnges

--- exporter/tmp.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-html = requests.get("http://127.0.0.1:8080/index.php/Main_Page?action=render").text
-soup = BeautifulSoup(html, "html.parser")
-
-for div in soup.find_all(True):
-    if div.get("id"):
-        print(div.name, div["id"])
